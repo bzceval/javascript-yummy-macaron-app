7091b1b03d4e2aa7dc1c926c8aa371908b3297a8
bzceval <busraaceval@gmail.com>
2022-10-13 11:20:39 +0300

    make control all event

--- app.js
+++ app.js
@@ -12,12 +12,22 @@ const macaronsDiv = document.querySelector(".macarons")
 
 macaronsDiv.addEventListener("click", (event) => {
     if(event.target.className == "bi bi-dash") {
-        console.log("minus")
+        // console.log(event.target.parentElement.parentElement.querySelector(".quantity").innerText)
+        if(event.target.parentElement.parentElement.querySelector(".quantity").innerText>1) {
+            event.target.parentElement.parentElement.querySelector(".quantity").innerText--;
+        } else {
+            console.log("değilse product-line-price içerisini sıfırla")
+        }
+        
     }
     if (event.target.classList.contains("bi-plus")) {
-        console.log("plus")
+        // console.log("plus")
+        // console.log(event.target.parentElement.nextElementSibling.innerText)
+        event.target.parentElement.nextElementSibling.innerText++; 
     }
     if(event.target.classList.contains("remove-product")) {
-        console.log("remove btn")
+        // console.log("remove btn")
+        event.target.closest(".macaron").remove()
+        // console.log(event.target.parentElement.parentElement.parentElement)
     }
 })
